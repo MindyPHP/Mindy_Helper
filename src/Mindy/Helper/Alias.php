48a957199732c31ad1dc303b15cafa0147f2d31e
Falaleev Maxim <max@studio107.ru>
2014-05-27 16:53:25 +0400

    Add empty Alias class and Meta helper

---
+++ src/Mindy/Helper/Alias.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 
+ *
+ * All rights reserved.
+ * 
+ * @author Falaleev Maxim
+ * @email max@studio107.ru
+ * @version 1.0
+ * @company Studio107
+ * @site http://studio107.ru
+ * @date 12/05/14.05.2014 18:25
+ */
+
+namespace Mindy\Helper;
+
+
+class Alias 
+{
+    /**
+     * @param $alias
+     * @return string
+     */
+    public static function get($alias)
+    {
+
+    }
+}
