5d180c27e68a73d58631d8ba4e1a1c419e75c50c
Falaleev Maxim <max@studio107.ru>
2014-07-18 20:57:57 +0400

    Add fastest method for read file

--- src/Mindy/Helper/File.php
+++ src/Mindy/Helper/File.php
@@ -34,6 +34,69 @@ use Mindy\Exception\Exception;
 class File
 {
     /**
+     * Original code: https://gist.github.com/lorenzos/1711e81a9162320fde20
+     * Article: http://stackoverflow.com/questions/15025875/what-is-the-best-way-in-php-to-read-last-lines-from-a-file
+     *
+     * @param $filepath
+     * @param int $lines
+     * @param bool $adaptive
+     * @return bool|string
+     */
+    public function readFile($filepath, $lines = 1, $adaptive = true)
+    {
+        // Open file
+        $f = @fopen($filepath, "rb");
+        if ($f === false) {
+            return false;
+        }
+
+        // Sets buffer size
+        if (!$adaptive) {
+            $buffer = 4096;
+        } else {
+            $buffer = ($lines < 2 ? 64 : ($lines < 10 ? 512 : 4096));
+        }
+
+        // Jump to last character
+        fseek($f, -1, SEEK_END);
+
+        // Read it and adjust line number if necessary
+        // (Otherwise the result would be wrong if file doesn't end with a blank line)
+        if (fread($f, 1) != "\n") {
+            $lines -= 1;
+        }
+
+        // Start reading
+        $output = '';
+        $chunk = '';
+
+        // While we would like more
+        while (ftell($f) > 0 && $lines >= 0) {
+            // Figure out how far back we should jump
+            $seek = min(ftell($f), $buffer);
+            // Do the jump (backwards, relative to where we are)
+            fseek($f, -$seek, SEEK_CUR);
+            // Read a chunk and prepend it to our output
+            $output = ($chunk = fread($f, $seek)) . $output;
+            // Jump back to where we started reading
+            fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
+            // Decrease our line counter
+            $lines -= substr_count($chunk, "\n");
+        }
+
+        // While we have too many lines
+        // (Because of buffer size we might have read too many)
+        while ($lines++ < 0) {
+            // Find first newline and remove all text before that
+            $output = substr($output, strpos($output, "\n") + 1);
+        }
+
+        // Close file and return
+        fclose($f);
+        return trim($output);
+    }
+
+    /**
      * Returns the extension name of a file path.
      * For example, the path "path/to/something.php" would return "php".
      * @param string $path the file path
@@ -190,6 +253,7 @@ class File
      * Level 0 means searching for only the files DIRECTLY under the directory;
      * level N means searching for those directories that are within N levels.
      * @param boolean $absolutePaths whether to return absolute paths or relative ones
+     * @throws \Mindy\Exception\Exception
      * @return array files found under the directory.
      */
     protected static function findFilesRecursive($dir, $base, $fileTypes, $exclude, $level, $absolutePaths)
