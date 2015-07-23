cb7d4fe14ed3c00bfd4e952fc015c7273283f6fb
Max107 <max@studio107.ru>
2015-07-23 20:15:46 +0300

    Added toUnderscore and toCamelCase methods

--- src/Mindy/Helper/Text.php
+++ src/Mindy/Helper/Text.php
@@ -72,4 +72,28 @@ class Text
     {
         return $needle === "" || mb_strrpos($haystack, $needle, 0, 'UTF-8') === mb_strlen($haystack, 'UTF-8') - mb_strlen($needle, 'UTF-8');
     }
+
+    /**
+     * Returns given word as CamelCased
+     * Converts a word like "send_email" to "SendEmail". It
+     * will remove non alphanumeric character from the word, so
+     * "who's online" will be converted to "WhoSOnline"
+     * @see variablize()
+     * @param string $word the word to CamelCase
+     * @return string
+     */
+    public static function toCamelCase($word)
+    {
+        return lcfirst(str_replace(' ', '', ucwords(preg_replace('/[^A-Za-z0-9]+/', ' ', $word))));
+    }
+
+    /**
+     * Converts any "CamelCased" into an "underscored_word".
+     * @param string $words the word(s) to underscore
+     * @return string
+     */
+    public static function toUnderscore($words)
+    {
+        return strtolower(preg_replace('/(?<=\\w)([A-Z])/', '_\\1', $words));
+    }
 }
